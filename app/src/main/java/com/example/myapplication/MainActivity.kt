c7ece01e3699025d664d3809d4e59eecddfe8ca3
ggfg <1321@gmail.com>
2022-10-04 21:35:17 +0800

    fffff

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,16 +7,27 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  etname:EditText
+    lateinit var  bt0k:Button
+    lateinit var tvName:TextView
+    lateinit var  btClear: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val edName = findViewById<EditText>(R.id.edName)
-        val bt0k = findViewById<Button>(R.id.button)
-        val tyName = findViewById<TextView>(R.id.tyName)
+        etname = findViewById(R.id.etName)
+        bt0k = findViewById(R.id.bt0k)
+        tvName = findViewById(R.id.tvName)
+        btClear = findViewById(R.id.btClear)
 
         bt0k.setOnClickListener{
-            tyName.text = edName.text
+            tvName.text = etname.text
         }
+
+            btClear.setOnClickListener {
+                tvName.text = ""
+            }
     }
 }
